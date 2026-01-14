874008b1e9116ef69229801b5b31a52dc2373539
Oscar Tu <oscartu20021217@gmail.com>
2026-01-13 20:04:10 -0500

    Create a window

---
+++ program.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "window.hpp"
+
+int main() {
+    std::cout << "Creating Window\n";
+
+    Window* pWindow = new Window();
+
+    bool running = true;
+    while (running) {
+        if (!pWindow->ProcessMessages()) {
+            std::cout << "Closing Window\n";
+            running = false;
+        }
+
+        // Render
+        Sleep(10);
+    }
+    delete pWindow;
+    return 0;
+}
